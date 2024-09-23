910a26d9dbe0f834e0a74f7bf0f3ee518427883c
Dhruv Patel <dhruvpatel.work@gmail.com>
2024-09-22 21:34:17 -0600

    chore: Reorganize project structure

--- app/root.tsx
+++ app/root.tsx
@@ -1,29 +1,6 @@
-import {
-  Links,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-import "./tailwind.css";
+import { Outlet } from "@remix-run/react";
 
-export function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-      </body>
-    </html>
-  );
-}
+export { Layout } from './components/Layout';
 
 export default function App() {
   return <Outlet />;
